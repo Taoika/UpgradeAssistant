f4cc32b5db7ab3f9701ad88ea197704e0e6981b3
kkt <670335957@qq.com>
2022-11-05 23:58:27 +0800

    init

---
+++ upgradeassistant/src/components/Nav/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.scss'
+
+export default function Nav() {
+  return (
+    <div>Nav</div>
+  )
+}
---
+++ upgradeassistant/src/routes/index.tsx
@@ -0,0 +1,10 @@
+import Home from "../pages/Home";
+
+const routes = [
+    {
+        path: '/Home',
+        element: <Home/>,
+    },
+];
+
+export default routes
