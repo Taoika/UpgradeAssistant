79ec0d326a5fcc96e005f87391f142d11d4b398a
FuriKuri <277541105@qq.cpom>
2022-11-08 13:10:24 +0800

    完成导航栏

--- upgradeassistant/src/App.tsx
+++ upgradeassistant/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import Home from './pages/Home';
-
+import { useRoutes } from 'react-router-dom'
+import routes from './routes'
+import './base.css'
+import Nav from './components/Nav'
 function App() {
+  const element = useRoutes(routes);
+  //导航栏
+  const data = [['home', '主页'], ['alldata', '所有资料'], ['test', '你好']]
+
   return (
     <div className="App">
-      <Home/>
+      <Nav data={data} />
+      {element}
     </div>
   );
 }
